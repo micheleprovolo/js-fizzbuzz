b1d14bf449c9e5abebf7c5e2546f1c4e21cdae28
micheleprovolo <michele.provolo@gmail.com>
2019-10-24 17:51:44 +0200

    ex ok

--- js/main.js
+++ js/main.js
@@ -11,11 +11,19 @@
 
 //stampo i numeri da 1 a 100;
 
-for (var i = 1; i <= 100; i++) {
-    console.log(i);
-    if(i % 3 == 0){
-        console.log("Fizz");
-    } 
+for (var i = 1; i <= 100; i++){
+        
+    if(i % 15 == 0){
+        console.log("FizzBuzz")
+
+    } else if (i % 5 == 0){
+        console.log("Buzz")
+
+    } else if (i % 3 == 0){
+        console.log("Fizz")
+
+    } else {console.log(i)
     }
 
+}
 
