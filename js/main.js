fb4d1817f914eb551b8f0cd9d23db910be0e46f9
micheleprovolo <michele.provolo@gmail.com>
2019-10-24 15:27:46 +0200

    ciclo numeri da 1 a 100

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
 // e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+//stampo i numeri da 1 a 100;
+
+for (var i = 1; i <= 100; i++) {
+    console.log(i);
+    
+    }
