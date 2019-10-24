cecd4a14eaa28e2cadde64b06756fa6662fce430
micheleprovolo <michele.provolo@gmail.com>
2019-10-24 17:17:52 +0200

    tentativo sostituzione fizz

--- js/main.js
+++ js/main.js
@@ -5,9 +5,17 @@
 // e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+
+// var multipliTre = false;
+// var multipliCinque = false;
+
 //stampo i numeri da 1 a 100;
 
 for (var i = 1; i <= 100; i++) {
     console.log(i);
-    
+    if(i % 3 == 0){
+        console.log("Fizz");
+    } 
     }
+
+
